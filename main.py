c1c289cb5632dcc864e9bef74bd8b72932472c04
Ulbandus <tovarishbozhanov@gmail.com>
2021-02-06 14:44:03 +0000

    Add resizing

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+from PyQt5.QtCore import Qt
 from ui_design import Ui_MainWindow
 
 SCREEN_SIZE = [600, 450]
@@ -13,11 +14,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.spn = '1,1'
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=30.304899,59.918068&spn=1,1&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll=30.304899,59.918068&spn={self.spn}&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -35,13 +37,38 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
-        ## Изображение
+        # Изображение
         self.pixmap = QPixmap(self.map_file)
         self.mapImage.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
+
+    def keyPressEvent(self, event) -> None:
+        if event.key() == Qt.Key_Up:
+            self.resize_handler('+')
+        elif event.key() == Qt.Key_Down:
+            self.resize_handler('-')
+
+    def resize_handler(self, button: str) -> None:
+        '''Handle resizing of map'''
+        spn = self.spn.split(',')
+        if button == '+':
+            spn = list(map(lambda x: str(round(float(x) / 2, 2)), spn))
+        elif button == '-':
+            spn = list(map(lambda x: str(round(float(x) * 2, 2)), spn))
+        if float(spn[0]) < 0:
+            spn[0] = '0.01'
+        if float(spn[1]) < 0:
+            spn[1] = '0.01'
+        self.spn = ','.join(spn)
+        self.update_main_image()
+
+    def update_main_image(self):
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.mapImage.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':
