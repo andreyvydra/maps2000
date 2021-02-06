c0b6c3ee205059acfa21a385b0ecd7c227d8bfca
shmonder-glitch <55046615+shmonder-glitch@users.noreply.github.com>
2021-02-06 17:32:11 +0300

    ADD address searching

--- main.py
+++ main.py
@@ -9,15 +9,56 @@ from ui_design import Ui_MainWindow
 SCREEN_SIZE = [600, 450]
 
 
+def geocode(address):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": address,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        # обработка ошибочной ситуации
+        pass
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    return toponym
+
+
+def get_coordinates(address):
+    toponym = geocode(address)
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return float(toponym_longitude), float(toponym_lattitude)
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.getImage()
+        self.search.clicked.connect(self.get_new_image_with_search)
         self.initUI()
 
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=30.304899,59.918068&spn=1,1&l=map"
+    def get_new_image_with_search(self):
+        toponym_to_find = self.searchEdit.text()
+        toponym_longitude, toponym_lattitude = get_coordinates(toponym_to_find)
+        ll = ",".join([str(toponym_longitude), str(toponym_lattitude)])
+        self.getImage(ll)
+        self.set_new_image()
+
+    def set_new_image(self):
+        self.pixmap = QPixmap(self.map_file)
+        self.mapImage.setPixmap(self.pixmap)
+
+    def getImage(self, ll='30.304899,59.918068'):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn=1,1&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -36,8 +77,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle('Отображение карты')
 
         ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.mapImage.setPixmap(self.pixmap)
+        self.set_new_image()
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
