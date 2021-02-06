19340fbd91c9f1b7323857b76b31a9e0255228cb
Ulbandus <tovarishbozhanov@gmail.com>
2021-02-06 15:11:04 +0000

    rebind keys and fix few bugs

--- main.py
+++ main.py
@@ -3,20 +3,46 @@ import sys
 
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtCore import Qt
-from ui_design import Ui_MainWindow
 
 SCREEN_SIZE = [600, 450]
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
         self.spn = '1,1'
         self.getImage()
         self.initUI()
+
+    def keyPressEvent(self, event) -> None:
+        if int(event.modifiers()) == Qt.ControlModifier:
+            if event.key() in [Qt.Key_Plus, Qt.Key_Equal]:
+                self.resize_handler('+')
+            elif event.key() == Qt.Key_Minus:
+                self.resize_handler('-')
+
+    def resize_handler(self, button: str) -> None:
+        '''Handle resizing of map'''
+        spn = self.spn.split(',')
+        if button == '+':
+            spn = list(map(lambda x: str(round(float(x) / 2, 2)), spn))
+        elif button == '-':
+            spn = list(map(lambda x: str(round(float(x) * 2, 2)), spn))
+        if float(spn[0]) < 0:
+            spn[0] = '0.01'
+        if float(spn[1]) < 0:
+            spn[1] = '0.01'
+        self.spn = ','.join(spn)
+        self.update_main_image()
+
+    def update_main_image(self):
+        self.getImage()
+        self.pixmap = QPixmap(self.map_file)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
 
     def getImage(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll=30.304899,59.918068&spn={self.spn}&l=map"
@@ -39,40 +65,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Изображение
         self.pixmap = QPixmap(self.map_file)
-        self.mapImage.setPixmap(self.pixmap)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
-    def keyPressEvent(self, event) -> None:
-        if event.key() == Qt.Key_Up:
-            self.resize_handler('+')
-        elif event.key() == Qt.Key_Down:
-            self.resize_handler('-')
-
-    def resize_handler(self, button: str) -> None:
-        '''Handle resizing of map'''
-        spn = self.spn.split(',')
-        if button == '+':
-            spn = list(map(lambda x: str(round(float(x) / 2, 2)), spn))
-        elif button == '-':
-            spn = list(map(lambda x: str(round(float(x) * 2, 2)), spn))
-        if float(spn[0]) < 0:
-            spn[0] = '0.01'
-        if float(spn[1]) < 0:
-            spn[1] = '0.01'
-        self.spn = ','.join(spn)
-        self.update_main_image()
-
-    def update_main_image(self):
-        self.getImage()
-        self.pixmap = QPixmap(self.map_file)
-        self.mapImage.setPixmap(self.pixmap)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MainWindow()
+    ex = Example()
     ex.show()
     sys.exit(app.exec())
